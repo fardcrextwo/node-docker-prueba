6104659314bbc8e8672c2480815bdea09af529b5
jair <jacobo_9410@hotmail.com>
2021-11-03 10:50:54 -0500

    Adding an article to week 02 of articles of the week

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "Hello World micado" });
+  res.json({ message: "Hello World desde una pull request" });
 });
 
 module.exports = router;
